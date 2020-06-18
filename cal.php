bda5d40670121c8a850081616db6805b87bfbb7d
Val++le <v.kummert@drmnt.de>
2020-06-18 02:11:01 +0200

    add bootstrap and table responsive class
    add bootstrap from official bootstrap cdn. Could also be implemented as local file on the webserver (just point to a bootstrap.min.css and js).
    Just made the table to a responsive bootstrap table so you can scroll through the table on small devices. Just improves the mobile usability and does not effect the desktop usability.

--- cal.php
+++ cal.php
@@ -174,6 +174,8 @@ function getOccupations(){
 <head>
  <link rel="stylesheet" type="text/css" href="./style.css">
  <script type=text/javascript src=func.js></script>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -209,7 +211,7 @@ function getOccupations(){
 </div>
 
 <button id="myBtn">How to occupy</button>
-<table class="cal">
+<table class="cal table-responsive">
 <?php
 print(tbl());
 ?>
